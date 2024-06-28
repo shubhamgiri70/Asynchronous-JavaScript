022fedeeb970fdc3feae59008ae8038b3429639b
Shubham Giri <shubhamgiri7631.sg@gmail.com>
2024-06-28 18:20:20 +0530

    practice promises

--- Assignment: Image Search App/index.js
+++ Assignment: Image Search App/index.js
@@ -1,19 +1,35 @@
 const url =
   "https://api.unsplash.com/photos/?client_id=jiqtwEKVXzmAalOZdhSyg1r0AZFDuP6WAb4bNa9c8qk";
+
 const getSearchUrl = (query) =>
   `https://api.unsplash.com/search/photos?query=${query}&client_id=jiqtwEKVXzmAalOZdhSyg1r0AZFDuP6WAb4bNa9c8qk`;
+
 const root = document.querySelector(".images");
 const searchEle = document.querySelector("input");
 
-function fetch(url, successHandler) {
-  let xhr = new XMLHttpRequest();
-  xhr.open("GET", url);
-  xhr.onload = () => successHandler(JSON.parse(xhr.response));
-  xhr.onerror = () => {
-    console.log("something went wrong!");
-  };
-  xhr.send();
-}
+// function fetch(url, successHandler) {
+//   let xhr = new XMLHttpRequest();
+//   let xhr2 = new XMLHttpRequest();
+
+//   xhr.open("GET", url);
+//   xhr.onload = () => successHandler(JSON.parse(xhr.response));
+//   xhr.onerror = () => {
+//     console.log("something went wrong!");
+//   };
+//   xhr.send();
+// }
+
+const imageRender = (url) => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.results.length) {
+        displayImage(res);
+      } else if (res.results){
+        displayImage(res);
+      }
+    });
+};
 
 function displayImage(images) {
   root.innerHTML = "";
@@ -26,13 +42,13 @@ function displayImage(images) {
   });
 }
 
-fetch(url, displayImage);
+imageRender(url);
 
 function handleSearch(event) {
   if (event.keyCode === 13 && searchEle.value) {
-    fetch(getSearchUrl(searchEle.value), function (searchResult) {
-      displayImage(searchResult.results);
-    });
+    const searchUrl = getSearchUrl(searchEle.value);
+
+    imageRender(searchUrl);
     searchEle.value = "";
   }
 }
